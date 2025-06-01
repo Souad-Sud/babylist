34f83a3f6fb3903dc38ac6c688c649c834c389bd
todatoday <taki.souad-90@hotmail.com>
2025-06-01 12:57:12 +0200

    wishlist

--- src/components/WishingList/index.tsx
+++ src/components/WishingList/index.tsx
@@ -1,9 +1,10 @@
-import './wishingList.scss';
+import "./wishingList.scss";
 
 const WishingList = () => {
   return (
-    <div>
-      widhong list
+    <div className="wishList">
+      <h2>Welcome to your wishing list. Please log in if you are a member.</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quas vero saepe, deleniti non, nobis mollitia autem assumenda excepturi eveniet eaque ipsam delectus nisi maiores, ipsum necessitatibus libero? Fuga, aliquid.</p>
     </div>
   );
 };
